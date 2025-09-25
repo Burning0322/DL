e18548e9056a15283ebdea6cb5f6f4c2007943c5
LOW REN HONG <83690213+Burning0322@users.noreply.github.com>
2025-09-25 17:24:54 +0800

    change batch size 20

--- Test3_vggnet/train.py
+++ Test3_vggnet/train.py
@@ -69,7 +69,7 @@ def main():
     loss_function = nn.CrossEntropyLoss()
     optimizer = optim.Adam(net.parameters(), lr=0.001)
 
-    epochs = 50
+    epochs = 20
     best_acc = 0.0
     save_path = './{}Net.pth'.format(model_name)
     train_steps = len(train_loader)
